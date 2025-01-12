857300220adcc71bff8334e1d6537f3d3a249b17
damiru2 <chalkarovd11@gmail.com>
2025-01-12 18:29:38 +0500

    add basic design as collection view, buttons and page control. make some constraints

--- Pexels/OnboardingViewController.swift
+++ Pexels/OnboardingViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLayoutSubviews() {
+        
+        skipButton.layer.cornerRadius = skipButton.frame.height / 2
+        nextButton.layer.cornerRadius = nextButton.frame.height / 2
+        
     }
-    */
+    
+    @IBAction func skipButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+    }
+    
 
 }
