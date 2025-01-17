a767b366362195d1c6af2f96b21c6d82f0a8bfe4
damiru2 <chalkarovd11@gmail.com>
2025-01-17 19:19:32 +0500

    realize collection view with scrolling, activate pagecontrol, activate next button

--- Pexels/OnboardingViewController.swift
+++ Pexels/OnboardingViewController.swift
@@ -7,20 +7,27 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+class OnboardingViewController: UIViewController, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var skipButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    var pages: [OnboardingModel] = []
+    var pages: [OnboardingModel] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.register(UINib(nibName: OnboardingCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: OnboardingCollectionViewCell.identifier)
         collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        generatePages()
 
     }
     
@@ -31,12 +38,40 @@ class OnboardingViewController: UIViewController {
         
     }
     
+    func generatePages() {
+        
+        pages = [
+            OnboardingModel(imageName: "onboarding1", title: "Play Anywhere", subTitle: "The video call feature can be accessed from anywhere in your house to help you."),
+            OnboardingModel(imageName: "onboarding2", title: "Stay Healthy", subTitle: "Nobody likes to be alone and the built-in group video call feature helps you connect."),
+            OnboardingModel(imageName: "onboarding3", title: "Make Connections", subTitle: "While working the app reminds you to smile, laugh, walk and talk with those who matters.")
+        ]
+        
+    }
+    
     @IBAction func skipButtonClicked(_ sender: UIButton) {
+        
+        start()
+        
     }
     
     @IBAction func nextButtonClicked(_ sender: UIButton) {
+        
+        if pageControl.currentPage == pageControl.numberOfPages - 1 {
+            start()
+        } else {
+            
+            pageControl.currentPage += 1
+            
+            let x: CGFloat = collectionView.frame.width * CGFloat(pageControl.currentPage)
+            collectionView.setContentOffset(CGPoint(x: x, y: 0), animated: true)
+            
+        }
+        
     }
     
+    func start() {
+        
+    }
 
 }
 
@@ -47,8 +82,28 @@ extension OnboardingViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        
+        let onboardingModel = pages[indexPath.item]
+        cell.setup(onboardingModel: onboardingModel)
+        
         return cell
     }
 }
 
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+    
+}
+
+extension OnboardingViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width)
+    }
+    
+}
 
